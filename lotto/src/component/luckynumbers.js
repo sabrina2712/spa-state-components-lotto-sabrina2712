d07baa62ee70bda2629733c194c8b53b3d88dbad
sabrina2712 <mailroshni@gmail.com>
2020-03-30 09:24:52 -0400

    add buttons with print

--- lotto/src/component/luckynumbers.js
+++ lotto/src/component/luckynumbers.js
@@ -4,15 +4,40 @@ class LuckyNumbers extends Component {
   constructor(props) {
     super(props);
 
-    this.createNumbers = this.createNumbers.bind(this);
+    // this.createNumbers = this.createNumbers.bind(this);
+
+    this.state = { numbers: this.createNumbers() };
   }
 
   createNumbers() {
-    let numbers = Math.floor(Math.random() * 10);
-    console.log(numbers);
+    return [1, 2, 3, 4, 5].map(ele => {
+      return Math.floor(Math.random() * 10);
+    });
   }
+  onResetClick = () => {
+    this.setState({});
+  };
+
+  onShow = () => {
+    this.setState({ numbers: this.createNumbers() });
+  };
+
   render() {
-    return <button className="input" onClick={this.createNumbers}></button>;
+    let numbers = this.state.numbers.map(num => {
+      return <div>{num}</div>;
+    });
+    console.log(numbers);
+    return (
+      <div>
+        {numbers}
+        <button className="resetButton" onClick={this.onResetClick}>
+          Reset
+        </button>
+        <button className="showButton" onClick={this.onShow}>
+          SHow me Result
+        </button>
+      </div>
+    );
   }
 }
 export default LuckyNumbers;
